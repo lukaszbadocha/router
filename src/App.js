e1155195fa31be6c7905b0e4842dbc7a6a90464d
lukaszbadocha <krakow@akademia108.pl>
2019-07-24 07:54:10 +0200

    init

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Kontakt</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="container">
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h1>Zadzwońże!</h1>
+      <p>tel: 606 745 759</p>
+    </div>
+  );
+}
+
+export default Contact;
